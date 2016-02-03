2fae0838f88bb7370c1d7ae7ab6de61460dc089d
Ricardo Romero <ricardojoseromero@gmail.com>
2016-02-03 16:29:58 -0500

    Adding all required files. No actual changes made

---
+++ top.c
@@ -0,0 +1,33 @@
+//top.c
+#include "types.h"
+#include "stat.h"
+#include "fs.h"
+#include "user.h"
+
+
+int main()
+{
+	struct uproc up[4];
+	int correct = 0;
+	int numprocs = 9;
+	int prev = 0;
+	printf(1, "In top\n");
+	//call getprocs, retrieve number of processes and allocate it using malloc
+	while(!correct)
+	{
+		//This allocates space for the array that we will be printing out
+		malloc(prev*sizeof(struct uproc));
+		//Call getprocs passing parameters
+		prev = numprocs;
+		numprocs = getprocs(prev,up);
+		// getprocs returns active processes
+		//Allocate more memory
+		printf(1,"%d",numprocs);
+
+		if(prev == numprocs)
+		{
+			correct = 1;
+		}
+	}
+	exit();
+}
